9c1224ef23afa773fb5b48bfd0a73ecc374a06d8
Joel Daniel Dick <joel.d.dick@gmail.com>
2015-04-26 14:07:01 -0700

    Init Commit 2

---
+++ proj2/src/proj2/EventThread.java
@@ -0,0 +1,13 @@
+package proj2;
+
+public class EventThread extends Thread{
+	private int siteNum;
+	
+	public EventThread(int siteNum){
+		this.siteNum = siteNum;
+	}
+	
+	public void run(){
+		// DO SOMETHING
+	}
+}
--- proj2/src/proj2/mainThread.java
+++ proj2/src/proj2/mainThread.java
@@ -1,5 +1,11 @@
 package proj2;
 
-public class mainThread {
-
+public class MainThread{
+	
+	public static void main(String[] args){
+		(new EventThread(1)).start();
+		(new EventThread(2)).start();
+		(new EventThread(3)).start();
+		(new EventThread(4)).start();
+	}
 }
